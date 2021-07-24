151f469137dce3749ff7f9efa55e0aade3658b47
siegaii <siiegaiii@gmail.com>
2021-07-24 11:07:41 +0800

    first commit

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,21 @@
+// nav
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '技术文档',
+    link: '/technology/',
+    items: [
+      { 
+        text: 'Node', 
+        items:[
+          {
+            text: '架构', 
+            link: '/pages/d4f127/' 
+          }
+        ]
+      },
+    ],
+  },
+  // { text: '开发笔记', link: '/note/' },
+  // { text: '日常', link: '/note/' },
+]
