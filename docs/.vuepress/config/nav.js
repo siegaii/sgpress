5b3025615d0b730105ab5e775cee72344ad9c9fd
siegaii <siiegaiii@gmail.com>
2021-12-20 17:49:20 +0800

    feat: 优化

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -5,32 +5,6 @@ module.exports = [
     text: '技术文档',
     link: '/technology/',
     items: [
-      // {
-      //   text: 'Javascript',
-      //   items: [
-      //     {
-      //       text: 'Js 实现柯里化',
-      //       link: '/pages/fb08d4/'
-      //     }
-      //   ]
-      // },
-      // {
-      //   text: 'Node',
-      //   items: [
-      //     {
-      //       text: '架构',
-      //       link: '/pages/668df1/'
-      //     },
-      //     {
-      //       text: '基础',
-      //       link: '/pages/4a37a8/'
-      //     },
-      //     {
-      //       text: '全局属性',
-      //       link: '/pages/d20b6a/'
-      //     },
-      //   ]
-      // },
       {
         text: '数据结构与算法',
         items: [
